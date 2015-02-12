5c015b27906955e3a33ea748e097121b681abc8b
Leandro Pessini <leandro@pessini.me>
2015-02-12 18:22:50 -0200

    Fixing some Auto-Layout issues

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -10,18 +10,68 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *cityTaxSegmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *errorLabel;
+
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // Setting up the cities taxes
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onCalculateButtonTapped:(id)sender {
+
+
+    // error message to help user
+    self.errorLabel.text = @"";
+
+    if ([self.priceTextField.text isEqualToString:@""]) {
+        self.errorLabel.text = @"You must enter a price.";
+    } else {
+
+        NSString *valueTextField = self.priceTextField.text;
+        double valueEntered = valueTextField.intValue;
+        double result=0.00;
+
+        if (self.cityTaxSegmentedControl.selectedSegmentIndex == 0) {
+
+            result = valueEntered * self.caTax;
+
+        } else if (self.cityTaxSegmentedControl.selectedSegmentIndex == 1){
+
+            result = valueEntered * self.chiTax;
+
+        } else if (self.cityTaxSegmentedControl.selectedSegmentIndex == 2) {
+
+            result = valueEntered * self.nyTax;
+
+            // if UISegmentedControl is -1 (not selected)
+        } else {
+
+            self.errorLabel.text = @"You must choose a city.";
+            
+        }
+        
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+        
+    }
+    
 }
 
 @end
